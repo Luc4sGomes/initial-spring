2412c1f3d239e1627b52dae7772f672ef2dbb92e
lucas <lucasgominhaa448@gmail.com>
2023-07-08 12:38:41 -0300

    added conversao dos dados, criação da classe dto e metodo save na classe de serviço

--- src/main/java/com/api/initialspring/services/ParkingSpotService.java
+++ src/main/java/com/api/initialspring/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.api.initialspring.services;
 
+import com.api.initialspring.models.ParkingSpotModel;
 import com.api.initialspring.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +12,10 @@ public class ParkingSpotService {
 
     public ParkingSpotService (ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
